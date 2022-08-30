766caf02ed873f5c659aaacdd84bdf5a38ca506c
Ilker Tacyildiz <tacyildizilker@gmail.com>
2022-08-30 13:44:37 +0300

    CryptoCurrency

---
+++ CryptoCurrency/Model/CryptoCurrency.swift
@@ -0,0 +1,15 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCurrency
+//
+//  Created by ILKER on 30.08.2022.
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable {
+    
+    let currency: String
+    let price: String
+    
+}
---
+++ CryptoCurrency/View/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CryptoCurrency
+//
+//  Created by ILKER on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        return cell
+    }
+
+}
+
